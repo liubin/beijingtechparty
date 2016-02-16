8bcb783fbdccaa28ab94c9a5d8a33b93b7d7820d
bin liu <liubin0329@gmail.com>
2016-02-16 22:01:38 +0800

    init

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/beijingtechparty/"
+http_images_path = "/beijingtechparty/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/beijingtechparty/fonts"
+css_dir = "public/beijingtechparty/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
